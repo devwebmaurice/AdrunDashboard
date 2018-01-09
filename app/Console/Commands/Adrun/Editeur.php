5903a9def5abf49bcfa01e5dd410c5a7d6e01a95
devwebmaurice <31951134+devwebmaurice@users.noreply.github.com>
2018-01-09 14:34:28 +0400

    Fix for Unknown Editeur
    This fix repair the bug for unknown Editeurs

--- app/Console/Commands/Adrun/Editeur.php
+++ app/Console/Commands/Adrun/Editeur.php
@@ -48,6 +48,20 @@ class Editeur extends Command
         $ADRUN_editeurs     = AdrunWebsiteModel::getInstance()->getEditeurList();
         $ADTECH_editeurs    = AdtechWebsiteModel::getInstance()->getADTECHWebsiteIDList()->return;
         
+        //var_dump( count($ADTECH_editeurs), count($ADRUN_editeurs) );
+        
+        foreach($ADRUN_editeurs as $editeur ):
+            
+            $data            = AdtechWebsiteModel::getInstance()->getEditeurById($editeur->id_adtech, 1 );
+            $input           = [];
+            $input['id']     = $editeur->id;
+            $input['name']   = mb_strtoupper($data->return->name);
+            $input['url']    = mb_strtoupper(str_replace(array('http://','https://','/'), '', $data->return->URL));
+            
+            AdrunWebsiteModel::getInstance()->updateEditeurStarter($input);
+            
+        endforeach;
+        
         
         
         
