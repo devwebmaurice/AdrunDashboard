0287651397db34f9bc7793d1a6b538e815c82a9c
devwebmaurice <31951134+devwebmaurice@users.noreply.github.com>
2018-01-30 10:32:24 +0400

    Modified Config[]

--- config/adrun.php
+++ config/adrun.php
@@ -57,10 +57,10 @@ return [
 //        'Caroline Legagneur'        => 'c.legagneur@adrun.re',
 //        'Virginie Cuinet'           => 'v.cuinet@adrun.re',
 //        'Laurent Trotzier'          => 'l.trotzier@adrun.re',
-//        'Yoann HAPOLD'              => 'devweb@adrun.re',
-//        'Linda Santoulangue'        => 'traffic@adrun.re',
+        'Yoann HAPOLD'              => 'devweb@adrun.re',
+        'Linda Santoulangue'        => 'traffic@adrun.re',
         'Jacques Rima'              => 'devwebmaurice@adrun.re',
-//        'Mara Natacha SEEVATHEEAN ' => 'm.seevatheean@nrj.mu',
+        'Mara Natacha SEEVATHEEAN ' => 'm.seevatheean@nrj.mu',
     ],
     
     'EMAIL' => [
